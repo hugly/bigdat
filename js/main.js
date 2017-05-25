b0721c079fe7531f8032b51300d554497a18474c
hugly <64617842@qq.com>
2017-05-25 17:21:57 +0800

    project init

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/**
+ * Created by hugly on 25/05/2017.
+ */
+$(document).ready(function(){
+
+});
+(function(){
+    var oMain = {
+        init:function(){
+            this.caculateShadow();
+            this.leftSilderAnimateFn();
+        },
+        caculateShadow:function(){
+            var height = $(window).height();
+
+            $('#container,.left-sidebar,.content,.right-silder').css({
+                height:height-60
+            });
+
+        },
+        leftSilderAnimateFn:function(){
+            var oLeftSilder = $('#left-silder');
+
+            oLeftSilder.on('click','div',function(){
+                var _this = $(this);
+                _this.next().slideToggle(function(){
+                    var hasClass = _this.find('.iconfont').hasClass('trans');
+                    if(hasClass){
+                        _this.find('.iconfont').removeClass('trans');
+                    }else{
+                        _this.find('.iconfont').addClass('trans');
+                    }
+                });
+            });
+        }
+    };
+
+    oMain.init();
+})();
