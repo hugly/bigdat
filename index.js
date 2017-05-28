55ee3cf0953445e59f2067ad0a0bef2342e57623
hugly <64617842@qq.com>
2017-05-28 18:19:44 +0800

    一些配置文件的修改

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 /**
  * Created by hugly on 27/05/2017.
  */
-// var http = require('http');
-// var path = require('path');
-// //引入我们自己编写的server模块
-// var server = require('./server.js');
-// http.createServer(function (request, response) {
-//     //以下的判断用来做简单的路由
-//     if (request.url == "/") {
-//         filePath = path.join(__dirname, '/html/main.html');
-//     } else {
-//         filePath = path.join(__dirname, '/html' + request.url);
-//     }
-//     server.serveStatic(response, filePath);
-// }).listen(/*侦听4000端口*/8090, function () {
-//     console.log("Server listening on port 8090");
-// });
-
 var http = require('http');
 var express = require('express');
 var app = express();
